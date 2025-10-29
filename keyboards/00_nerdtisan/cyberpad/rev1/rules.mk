96f7ef15c772db1eec017d17bd90217fd2728e9a
Aiddy81 <21276456+Aiddy81@users.noreply.github.com>
2025-10-30 10:32:40 +1100

    removed unused code

--- keyboards/00_nerdtisan/cyberpad/rev1/rules.mk
+++ keyboards/00_nerdtisan/cyberpad/rev1/rules.mk
@@ -8,32 +8,6 @@ BOOTLOADER = rp2040
 CUSTOM_MATRIX = lite
 SRC += matrix.c
 
-#LTO_ENABLE = yes
-#BOOTMAGIC_ENABLE = yes
-#MOUSEKEY_ENABLE = yes
-#EXTRAKEY_ENABLE = yes       # Required for media keys
-#CONSOLE_ENABLE = no
-#COMMAND_ENABLE = no
-#NKRO_ENABLE = yes
-# Bootloader selection
-
-#ANALOG_DRIVER_REQUIRED = yes
-#ENCODER_ENABLE = yes
-#RGBLIGHT_ENABLE = yes    # Enable RGB lighting
-#RGBLED_OPTION_TYPE = backlight
-#WPM_ENABLE = yes
-
-#OLED_ENABLE = yes
-#oled_driver = yes
-#OLED_FONT_H = "keyboards/00_nerdtisan/cyberpad/lib/glcdfont.c"
-#OLED_DRIVER = ssd1306
-#WS2812_DRIVER = vendor
-#WS2812_DI_PIN = GP8
-#RGBLIGHT_LED_COUNT = 9 # Number of LEDs
-#I2C_DRIVER_REQUIRED = yes
-
-#MCUCONF = keyboards/00_nerdtisan/cyberpad/rev1/mcuconf.h
-
 # Build Options
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
